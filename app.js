f4740085ad99e27107d09e0b0296f900ead33385
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-08-21 18:06:04 -0700

    added dynamic search functionality for google maps geolocator api

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const urlGoogle = 'http://maps.googleapis.com/maps/api/geocode/json?address='
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Adress to fecth the weather for',
+            string: true
+        }
+})
+.help()
+.alias('help', 'h')
+.argv;
+const urlEncoded = encodeURIComponent(argv.a);
+
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=9028%20east%20lupine%20avenue',
+    url: urlGoogle + "" + urlEncoded,
     json: true
 }, (error, response, body) =>{
-console.log(body);
-});
+    if(error){
+        return error
+    }else{
+   console.log(`${body.results[0].formatted_address}
+   ${body.results[0].geometry.location.lat}
+   ${body.results[0].geometry.location.lng}`)
+
+}});
