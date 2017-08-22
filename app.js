244711472a24618483315f6ab4f4a885024bf761
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-08-21 20:06:33 -0700

    added error handling for the geolocation api call

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode')
 
-const urlGoogle = 'http://maps.googleapis.com/maps/api/geocode/json?address='
+// const urlGoogle = 
 const argv = yargs
     .options({
         a: {
@@ -14,17 +15,25 @@ const argv = yargs
 .help()
 .alias('help', 'h')
 .argv;
-const urlEncoded = encodeURIComponent(argv.a);
+let urlEncoded = encodeURIComponent(argv.a);
 
 
 request({
-    url: urlGoogle + "" + urlEncoded,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${urlEncoded}`,
     json: true
-}, (error, response, body) =>{
-    if(error){
-        return error
-    }else{
-   console.log(`${body.results[0].formatted_address}
+}, (error, response, body) => {
+ 
+    if (error)
+    {
+        console.log('unable to connect to the google servers, sorry :(');
+    }
+    else if (body.status === 'ZERO_RESULTS')
+    {
+        console.log('Unable to find that address.')
+    }
+    else if(body.status === 'OK')
+    {   
+    console.log(`${body.results[0].formatted_address}
    ${body.results[0].geometry.location.lat}
    ${body.results[0].geometry.location.lng}`)
 
